a1dce9f49faaf78cb1c9d1e48eb700b386bea4b0
GrishilovVladislav <79422175+GrishilovVladislav@users.noreply.github.com>
2021-04-22 14:57:16 +0300

    Add files via upload

---
+++ Lab.05/Lab.03/CoVidPatient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Lab._05
+{
+    class CoVidPatient : Patient
+    {
+        public int DaysOfStaying { get; set; }
+        public string ImportantInfo { get; set; }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("\n!WARNING!");
+            Console.WriteLine("!COVID PATIENT!\n");
+            Console.WriteLine($"DaysOfStaying: {DaysOfStaying}");
+            Console.WriteLine($"ImportantInfo: {ImportantInfo}");
+
+
+        }
+    }
+
+}
---
+++ Lab.05/Lab.03/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab._05
+{
+    class Patient : Human
+    {
+        public enum Deseases
+        {
+            Infections,
+            MechanicalDamage,
+        }
+        public enum CorpusNumbers
+        {
+            FirstCorpus,
+            SecondCorpus,
+            ThirdCorpus,
+        }
+        public int PatientNumber { get; set; }
+        public int ChamblerNumber { get; set; }
+        public Deseases Desease { get; set; }
+        public CorpusNumbers CorpusNumber { get; set; }      
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("\nPatinetInfo: ");
+            Console.WriteLine($"CorpusNumber: {CorpusNumber}");
+            Console.WriteLine($"ChamblerNumber: {ChamblerNumber}");
+            Console.WriteLine($"Desease: {Desease}");
+            Console.WriteLine($"PatientNumber: {PatientNumber}");
+        }
+    }
+}
