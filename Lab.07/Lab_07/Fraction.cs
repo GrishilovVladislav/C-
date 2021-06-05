922978889addbd47e065509ffeb1bab49774476b
GrishilovVladislav <79422175+GrishilovVladislav@users.noreply.github.com>
2021-06-05 09:43:10 +0300

    Add files via upload

---
+++ Lab.07/Lab_07/Fraction.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Fraction : IComparable<Fraction>, IEquatable<Fraction>
+{
+    private long Top { get; set; }
+    private long Bottom { get; set; } 
+    public Fraction(long top, long bottom)
+    {
+        Top = top;
+        Bottom = bottom;
+    }
+    public Fraction(long top)
+    {
+        Top = top;
+        Bottom = 1;
+    }
+    public Fraction()
+    {
+        Top = 0;
+        Bottom = 1;
+    }
+   
+    public double GetFraction()
+    {
+        return (double) (Top / Bottom);
+    }
+    public override string ToString()
+    {
+        return (Top.ToString() + "/" + Bottom.ToString());
+    }
+    public static bool TryParse(string N, out Fraction f)
+    {
+        f = null;
+        Regex pattern1 = new Regex(@"^(\d*)[/](\d*)$");
+        Regex pattern2 = new Regex(@"^\d*$");
+        Regex pattern3 = new Regex(@"^(\d*)[.,](\d*)$");
+        if (pattern1.IsMatch(N))
+        {
+            string[] words = N.Split(new char[] { '/' });
+            long numerator = long.Parse(words[0]);
+            long denumerator = long.Parse(words[1]);
+            f = new Fraction(numerator, denumerator);
+            return true;
+        }
+        else if (pattern2.IsMatch(N))
+        {
+            long numerator = long.Parse(N);
+            long denumerator = 1;
+            f = new Fraction(numerator, denumerator);
+            return true;
+        }
+        else if (pattern3.IsMatch(N))
+        {
+            string[] words = N.Split(new char[] { '.', ',' });
+            long numerator = long.Parse(words[0]);
+            long denumerator = long.Parse(words[1]);
+            f = new Fraction(numerator, denumerator);
+            return true;
+        }
+        else
+            return false;
+    }
+    public static Fraction Parse(string N)
+    {
+        Fraction F;
+        if (TryParse(N, out F))
+            return F;
+        throw new ArgumentException("Incorrect input");
+    }
+
+    public bool Equals(Fraction N)
+    {
+        if (N == null)
+            return false;
+        else
+            return (Top * N.Bottom == N.Top * Bottom);
+    }
+    public int CompareTo(Fraction N)
+    {
+        return ((Top * N.Bottom).CompareTo(N.Top * Bottom));
+    }
+
+    public double ToDouble(IFormatProvider provider)
+    {
+        return GetFraction();
+    }
+
+    public short ToInt16(IFormatProvider provider)
+    {
+        return Convert.ToInt16(GetFraction());
+    }
+
+    public int ToInt32(IFormatProvider provider)
+    {
+        return Convert.ToInt32(GetFraction());
+    }
+
+    public long ToInt64(IFormatProvider provider)
+    {
+        return Convert.ToInt64(GetFraction());
+    }
+    public decimal ToDecimal(IFormatProvider provider)
+    {
+        return Convert.ToDecimal(GetFraction());
+    }
+
+    public static Fraction operator +(Fraction N1, Fraction N2)
+    {
+        return new Fraction(N1.Top * N2.Bottom + N2.Top * N1.Bottom, N1.Bottom * N2.Bottom);
+    }
+    public static Fraction operator -(Fraction N1, Fraction N2)
+    {
+        return new Fraction(N1.Top * N2.Bottom - N2.Top * N1.Bottom, N1.Bottom * N2.Top);
+    }
+    public static Fraction operator *(Fraction N1, Fraction N2)
+    {
+        return new Fraction(N1.Top * N2.Top, N1.Bottom * N2.Bottom);
+    }
+    public static Fraction operator /(Fraction N1, Fraction N2)
+    {
+        return new Fraction(N1.Top * N2.Bottom, N1.Bottom * N2.Top);
+    }
+
+    public static bool operator <(Fraction N1, Fraction N2)
+    {
+        return (N1.Top * N2.Bottom < N1.Bottom * N2.Top);
+    }
+    public static bool operator >(Fraction N1, Fraction N2)
+    {
+        return (N1.Top * N2.Bottom > N1.Bottom * N2.Top);
+    }
+    public static bool operator !=(Fraction N1, Fraction N2)
+    {
+        return (N1.Top * N2.Bottom != N1.Bottom * N2.Top);
+    }
+    public static bool operator ==(Fraction N1, Fraction N2)
+    {
+        return (N1.Top * N2.Bottom == N1.Bottom * N2.Top);
+    }
+    public static bool operator <=(Fraction N1, Fraction N2)
+    {
+        return (N1.Top * N2.Bottom <= N1.Bottom * N2.Top);
+    }
+    public static bool operator >=(Fraction N1, Fraction N2)
+    {
+        return (N1.Top * N2.Bottom >= N1.Bottom * N2.Top);
+    }
+
+    public long GCD(long N1, long N2)
+    {
+        if (N1 > N2)
+        {
+            (N1, N2) = (N2, N1);
+        }
+        while (N2 != 0)
+        {
+            N1 %= N2;
+            (N1, N2) = (N2, N1);
+        }
+        long Res = N1;
+        return Res;
+    }
+}
---
+++ Lab.07/Lab_07/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace Lab_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction F1 = new Fraction(100, 77);
+            Fraction F2 = new Fraction(33, 56);
+            Fraction Res = new Fraction();
+            Res = F1 + F2;
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}");
+            Res = F1 - F2;
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}");
+            Res = F1 * F2;
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}");
+            Res = F1 / F2;
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}");
+      
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {F1 > F2}");
+
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {F1 < F2}");
+
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {F1 == F2}");
+
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {F1 != F2}");
+
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {F1 >= F2}");
+
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {F1 <= F2}");
+            
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res.ToString()}");
+
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res.GetFraction()}");
+            Res = Fraction.Parse("10");
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}" );
+            Res = Fraction.Parse("1/90");
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}");
+            Res = Fraction.Parse("999,99");
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}");
+            Res = Fraction.Parse("123123.123123443");
+            Console.WriteLine("=================================\n");
+            Console.WriteLine($"Result: {Res}");
+
+        }
+    }
+}
